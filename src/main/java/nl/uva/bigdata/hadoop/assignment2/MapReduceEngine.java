483999e8bde27c77ab6a6807a479bb4061f0d5fb
DebenOldert <debenoldert@me.com>
2021-03-05 00:46:01 +0100

    Complete Assignment 2 - Task 2

--- src/main/java/nl/uva/bigdata/hadoop/assignment2/MapReduceEngine.java
+++ src/main/java/nl/uva/bigdata/hadoop/assignment2/MapReduceEngine.java
@@ -35,30 +35,96 @@ public class MapReduceEngine<K1 extends Comparable<K1>, V1, K2 extends Comparabl
             Collection<Record<K1, V1>> inputRecords,
             MapFunction<K1, V1, K2, V2> map
     ) {
-        //TODO Implement me
-        throw new IllegalStateException("Not implemented");
+        //READ DATA
+
+        Collection<Record<K2, V2>> result = null;
+
+        for(Record<K1, V1> r: inputRecords){
+            System.out.println(r.key + " => " + r.value);
+            if(result == null){
+                result = map.map(r);
+            }
+            else{
+                result.addAll(map.map(r));
+            }
+        }
+        System.out.println("===========");
+
+        return result;
     }
 
     private Collection<Collection<Record<K2, V2>>> partitionMapOutputs(
             Collection<Record<K2, V2>> mapOutputs,
             int numPartitions) {
+        //Split collection
 
-        //TODO Implement me
-        throw new IllegalStateException("Not implemented");
+        int size = mapOutputs.size();
+
+        ArrayList<Collection<Record<K2, V2>>> parts = new ArrayList<>();
+
+//        for(int offset=0; offset<numPartitions; offset++){
+//            parts.set(offset, new ArrayList<Record<K2, V2>>());
+//            for(int i=offset; i<size; i+=numPartitions){
+//                parts.get(offset).add(mapOutputs)
+//            }
+//        }
+
+        int offset = 0;
+        for(Record<K2, V2> r: mapOutputs){
+            System.out.println(r.key + " => " + r.value);
+            if(offset >= numPartitions){
+                offset = 0;
+            }
+            if(offset >= parts.size()){
+                parts.add(new ArrayList<Record<K2, V2>>());
+            }
+            parts.get(offset).add(r);
+            offset++;
+        }
+
+        System.out.println("Parts: "+numPartitions);
+        System.out.println("===========");
+
+        return parts;
+
+        //throw new IllegalStateException("Not implemented");
     }
 
     private Map<K2, Collection<V2>> groupReducerInputPartition(Collection<Record<K2, V2>> reducerInputPartition) {
 
-        //TODO Implement me
-        throw new IllegalStateException("Not implemented");
+        //Group keys
+
+        Map<K2, Collection<V2>> groups = new HashMap<>();
+
+        for(Record<K2, V2> r: reducerInputPartition){
+            if(!groups.containsKey(r.key)){
+                groups.put(r.key, new ArrayList<V2>());
+            }
+            groups.get(r.key).add(r.value);
+        }
+
+        return groups;
+
+        //throw new IllegalStateException("Not implemented");
     }
 
     private Collection<Record<K2, V3>> runReducePhaseOnPartition(
             Map<K2, Collection<V2>> reducerInputs,
             ReduceFunction<K2, V2, V3> reduce
     ) {
-        //TODO Implement me
-        throw new IllegalStateException("Not implemented");
+        ArrayList<Record<K2, V3>> results = new ArrayList<>();
+
+        for(Map.Entry<K2, Collection<V2>> e: reducerInputs.entrySet()){
+            results.addAll(reduce.reduce(e.getKey(), e.getValue()));
+        }
+
+        for(Record<K2, V3> r: results){
+            System.out.println(r.key + " => " + r.value);
+        }
+
+        return results;
+
+        //throw new IllegalStateException("Not implemented");
     }
 
     public Collection<Record<K2, V3>> compute(
